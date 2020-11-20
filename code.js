25bafa963eb53a441343bc2e07d5c15006b4fee5
Dibyayan8992 <74058757+Dibyayan8992@users.noreply.github.com>
2020-11-20 12:00:10 +0530

    Add files via upload

---
+++ code.js
@@ -0,0 +1,121 @@
+var p5Inst = new p5(null, 'sketch');
+
+window.preload = function () {
+  initMobileControls(p5Inst);
+
+  p5Inst._predefinedSpriteAnimations = {};
+  p5Inst._pauseSpriteAnimationsByDefault = false;
+  var animationListJSON = {"orderedKeys":[],"propsByKey":{}};
+  var orderedKeys = animationListJSON.orderedKeys;
+  var allAnimationsSingleFrame = false;
+  orderedKeys.forEach(function (key) {
+    var props = animationListJSON.propsByKey[key];
+    var frameCount = allAnimationsSingleFrame ? 1 : props.frameCount;
+    var image = loadImage(props.rootRelativePath, function () {
+      var spriteSheet = loadSpriteSheet(
+          image,
+          props.frameSize.x,
+          props.frameSize.y,
+          frameCount
+      );
+      p5Inst._predefinedSpriteAnimations[props.name] = loadAnimation(spriteSheet);
+      p5Inst._predefinedSpriteAnimations[props.name].looping = props.looping;
+      p5Inst._predefinedSpriteAnimations[props.name].frameDelay = props.frameDelay;
+    });
+  });
+
+  function wrappedExportedCode(stage) {
+    if (stage === 'preload') {
+      if (setup !== window.setup) {
+        window.setup = setup;
+      } else {
+        return;
+      }
+    }
+// -----
+
+var thief=createSprite(10,390,10,10);
+thief.shapeColor="grey";
+var  laser1=createSprite(100,0,200,5);
+laser1.shapeColor="red";
+laser1.velocityY=2;
+var laser2=createSprite(300,400,200,5);
+laser2.shapeColor="red";
+laser2.velocityY=-2;
+
+
+
+function draw() {
+  background("white");
+  shape(390,0,380,10,390,20,400,10);
+  
+if (keyDown(RIGHT_ARROW)){
+  thief.velocityX=2;
+  thief.velocityY=0;
+}
+if (keyDown(LEFT_ARROW)){
+  thief.velocityX=-2;
+  thief.velocityY=0;
+  
+}
+if ( keyDown(UP_ARROW)){
+  thief.velocityX=0;
+  thief.velocityY=-2;
+}
+if (keyDown(DOWN_ARROW)){
+  thief.velocityX=0;
+  thief.velocityY=2;
+}
+
+if (laser1.isTouching(thief) || laser2.isTouching(thief)) {
+  stroke(0);
+  fill(0);
+  textSize(24);
+  text("Thief is caught",120,200);
+  laser1.setVelocity(0,0);
+  laser2.setVelocity(0,0);
+  thief.setVelocity(0,0);
+}
+createEdgeSprites();
+laser1.bounceOff(topEdge);
+laser1.bounceOff(bottomEdge);
+laser2.bounceOff(topEdge);
+laser2.bounceOff(bottomEdge);
+
+drawSprites();
+}
+
+
+
+
+
+  
+
+
+
+
+
+
+    
+  
+    
+
+
+// -----
+    try { window.draw = draw; } catch (e) {}
+    switch (stage) {
+      case 'preload':
+        if (preload !== window.preload) { preload(); }
+        break;
+      case 'setup':
+        if (setup !== window.setup) { setup(); }
+        break;
+    }
+  }
+  window.wrappedExportedCode = wrappedExportedCode;
+  wrappedExportedCode('preload');
+};
+
+window.setup = function () {
+  window.wrappedExportedCode('setup');
+};
